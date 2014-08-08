2bfc2fd2c6d514eaec9b6db46385d1754a98f9b7
Wentao Deng <wdeng@groupon.com>
2014-08-08 16:49:16 -0700

    Rain water

--- algorithms/RainWater.cpp
+++ algorithms/RainWater.cpp
@@ -14,5 +14,35 @@ using namespace std;
 
 int main()
 {
-    cout << "Heelo." << endl;
+    int board[] = {3, 14, 5, 4, 9, 2, 3, 5, 2, 11};
+    int size = sizeof(board)/sizeof(board[0]);
+    int* leftMax = new int [size];
+    int* rightMax = new int [size];
+    
+    int max=0;
+    for(int i=0;i<size;i++)
+    {
+        leftMax[i] = max;
+        if(board[i]>max)
+            max = board[i];
+    }
+    
+    max=0;
+    for(int i=size-1;i>=0;i--)
+    {
+        rightMax[i] = max;
+        if(board[i]>max)
+            max = board[i];
+    }
+    
+    int sum = 0;
+    for(int i=0;i<size;i++)
+    {
+        int level = (leftMax[i] < rightMax[i] ? leftMax[i] : rightMax[i]);
+        if(level - board[i] >0)
+            sum += (level - board[i]);
+    }
+    
+    cout << sum << endl;
+    
 }
