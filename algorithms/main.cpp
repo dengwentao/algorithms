6f2c5f19126f36f42fb493b6456e6d2ad92359fc
Wentao Deng <wdeng@groupon.com>
2014-08-08 15:38:45 -0700

    initialized

---
+++ algorithms/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  algorithms
+//
+//  Created by Wentao Deng on 8/8/14.
+//  Copyright (c) 2014 WENTAO INC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
