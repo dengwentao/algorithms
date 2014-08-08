5ddd25ad1c083c1e107326c76435ee021bdb2266
Wentao Deng <wdeng@groupon.com>
2014-08-08 15:55:15 -0700

    init

---
+++ algorithms/RainWater.cpp
@@ -0,0 +1,18 @@
+//
+//  RainWater.cpp
+//  algorithms
+//
+//  Created by Wentao Deng on 8/8/14.
+//  Copyright (c) 2014 WENTAO INC. All rights reserved.
+//
+//  Given an array of int values as board height. How much water can it hold?
+
+#include <iostream>
+using namespace std;
+#include <assert.h>
+#include "RainWater.h"
+
+int main()
+{
+    cout << "Heelo." << endl;
+}
