c72fbd34982da04a7128e64f3f7a62889d464dac
Wentao Deng <wdeng@groupon.com>
2014-09-09 12:03:58 -0700

    Largest BST subtree

--- algorithms/LargestBST.cpp
+++ algorithms/LargestBST.cpp
@@ -21,7 +21,7 @@ struct Node
     Node(int v){value=v; pLeft=pRight=0;};
 };
 
-// count how many nodes are in this BST (may not include all decendents)
+// count how many nodes are in this BST (may not include all descendents)
 int MAX_COUNT = 0;
 int countBst(Node* pRoot, int min, int max)
 {
@@ -46,6 +46,41 @@ int countBst(Node* pRoot, int min, int max)
     return count;
 }
 
+
+// This is for another similar problem.
+// This problem requires BST to have all its descendants.
+int max_subtree_count = 0;
+bool isBst(Node* pRoot, int min, int max, int& count)
+{
+    count = 0;
+    if(!pRoot)
+        return true;
+    int count_left = 0;
+    int count_right = 0;
+    if(pRoot->value >= min && pRoot->value <= max
+       && isBst(pRoot->pLeft, min, pRoot->value, count_left)
+       && isBst(pRoot->pRight, pRoot->value, max, count_right))
+    {
+        count = count_left + count_right + 1;
+        if(count > max_subtree_count)
+        {
+            max_subtree_count = count;
+            cout << pRoot->value << " has count " << max_subtree_count << endl;
+        }
+        return true;
+    }
+    else
+    {
+        isBst(pRoot->pLeft, INT_MIN, INT_MAX, count_left);
+        if(count_left > max_subtree_count)
+            max_subtree_count = count_left;
+        isBst(pRoot->pRight, INT_MIN, INT_MAX, count_right);
+        if(count_right > max_subtree_count)
+            max_subtree_count = count_right;
+        return false;
+    }
+}
+
 int main()
 {
     Node* pRoot = new Node(15);
@@ -61,5 +96,9 @@ int main()
     
     countBst(pRoot, INT_MIN, INT_MAX);
     cout << "Root total count is " << MAX_COUNT << endl;
+    
+    int count = 0;
+    isBst(pRoot, INT_MIN, INT_MAX, count);
+    
     return 0;
 }
